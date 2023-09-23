48254d3ccb8ff50d2494583edd75ef60c2fdb6c3
ARFA79 <arfa79lg@gmail.com>
2023-09-23 09:50:54 +0330

    python code

---
+++ githubbot.py
@@ -0,0 +1,15 @@
+from github import Github
+
+# Replace with your personal access token
+token = "YOUR_PERSONAL_ACCESS_TOKEN"
+
+def main():
+    g = Github(token)
+    user = g.get_user()
+    notifications = user.get_notifications()
+    
+    for notification in notifications:
+        print(notification.subject.title)
+
+if __name__ == '__main__':
+    main()
